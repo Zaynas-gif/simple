e8bd8dbf0935caa5f5774d8e925b3bb7967683dc
Zaynas <45643593+Lolikas12@users.noreply.github.com>
2020-11-06 10:26:41 +0100

    short about me

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+var person = {
+    firstName: "Jonas",
+    lastName : "Mikutavicius",
+    language : "Svenska/Engelska/Litausika",
+    age: 25,
+    hobbies: "Kampsport/Böcker/Datorspelr",
+    gatan: "Tranemansgatan 21B",
+    stad: "Helsingborg",
+
+
+
+
+    get namn() {
+      return this.firstName;
+      
+    },
+
+    get efternamn () {
+        return this.lastName
+    },
+
+    get ar() {
+        return this.age
+    },
+
+    get lang() {
+        return this.language
+
+    },
+
+    get hobb() {
+        return this.hobbies
+    },
+
+    get gat () {
+        return this.gatan
+    },
+
+    get stan () {
+        return this.stad
+    }
+
+    
+
+
+  };
+  
+  document.getElementById("name").innerHTML = person.namn + " " + person.efternamn;
+  document.getElementById("old").innerHTML = "Jag är :" + " " + person.age;
+  document.getElementById("stan").innerHTML = person.gatan + ", " + person.stad
+  document.getElementById("sprak").innerHTML = person.lang
+  document.getElementById("hobbies").innerHTML = "Hobbies :" + " " + person.hobb
+
