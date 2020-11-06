7f01deadd831850e7291e9e254b4fb1518c484fe
Zaynas <45643593+Lolikas12@users.noreply.github.com>
2020-11-06 13:26:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ var person = {
   };
   
   document.getElementById("name").innerHTML = person.namn + " " + person.efternamn;
-  document.getElementById("old").innerHTML = "Jag är :" + " " + person.age + "år gammal";
+  document.getElementById("old").innerHTML = "Jag är :" + " " + person.age + " " + "år gammal";
   document.getElementById("stan").innerHTML = "Plats:" + " " + person.gatan + ", " + person.stad
   document.getElementById("sprak").innerHTML = "Talar:" + " " + person.lang
   document.getElementById("hobbies").innerHTML = "Hobbies :" + " " + person.hobb
